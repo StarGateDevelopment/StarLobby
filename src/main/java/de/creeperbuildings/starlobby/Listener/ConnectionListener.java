2319a231390cd475d2c3a84ad705faacf9cfbe71
Julian Weinelt <julian.weinelt@outlook.de>
2023-05-08 19:34:12 +0200

    Scoreboard Ability

--- src/main/java/de/creeperbuildings/starlobby/Listener/ConnectionListener.java
+++ src/main/java/de/creeperbuildings/starlobby/Listener/ConnectionListener.java
@@ -1,5 +1,7 @@
 package de.creeperbuildings.starlobby.Listener;
 
+import de.creeperbuildings.starlobby.Main;
+import de.creeperbuildings.starlobby.scoreboard.ScoreboardManager;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,12 +13,16 @@ public class ConnectionListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        p.setGameMode(GameMode.ADVENTURE);
-        e.setJoinMessage("§7[§a+§7]" + p.getName());
+        e.setJoinMessage(Main.plugin().getConfig().getString("join-message"));
+
+        if (Main.plugin().getConfig().getBoolean("scoreboard.enabled")) {
+            Main.getScoreboardManager().createScoreboard(e.getPlayer());
+        }
+
     }
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
-        e.setQuitMessage("§7[§c-§7]" + p.getName());
+        e.setQuitMessage(Main.plugin().getConfig().getString("leave-message"));
     }
 }
